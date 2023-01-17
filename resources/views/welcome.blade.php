254285d51f16302924f856f46aa927907f52cc25
Ashish Wagle <Awagle4@gmail.com>
2023-01-17 15:57:22 +0545

    little changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,14 +73,41 @@
     </div>
 
     <footer>
-  <div class="foot">
-    <p>Copyright ©2022, Everest Philatelic Society</p>
-    <nav>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Contact</a></li>
-      </ul>
-    </nav>
+  <div class="containerfooter">
+    <div class="row">
+      <div class="col-md-3">
+        <h4>Location</h4>
+        <p>123 Main St</p>
+        <p>Pokhara, Nepal 12345</p>
+      </div>
+      <div class="col-md-3">
+        <h4>Social Links</h4>
+        <ul>
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">Twitter</a></li>
+          <li><a href="#">Instagram</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <h4>About</h4>
+        <ul>
+          <li><a href="#">Company</a></li>
+          <li><a href="#">Team</a></li>
+          <li><a href="#">Careers</a></li>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <h4>Contact</h4>
+        <ul>
+          <li><a href="#">Contact Us</a></li>
+          <li><a href="#">FAQs</a></li>
+          <li><a href="#">Blogs</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="copyright">
+      <p>Copyright ©2022 Everest Philatelic Society</p>
+    </div>
   </div>
 </footer>
+
