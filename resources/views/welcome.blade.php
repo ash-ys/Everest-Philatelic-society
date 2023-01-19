6a828eca214eb74fe1849aa1fcf85170428d75d7
Ashish Wagle <Awagle4@gmail.com>
2023-01-19 12:25:47 +0545

    hamburger

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script type="text/javascript" src="{{ asset('js/script.js') }}"></script>
     <link rel="Stylesheet" href="{{asset('style.css')}}">
     <title>Everest philatelic society</title>
 </head>
